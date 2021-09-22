730982e8173c7cfee84e05f428e153c4748176c4
Hector Liceaga <lice2187@gmail.com>
2021-09-22 18:53:13 -0300

    functions added: to binary, blur, gray scale

--- js/paint.js
+++ js/paint.js
@@ -120,7 +120,7 @@ document.getElementById("myImg").addEventListener('change', () => {
 let btnReload = document.getElementById("btnReloadImg");
 btnReload.addEventListener("click", reload);
 
-function reload(){
+function reload() {
     clean();
     canvas.width = backUpImg.width;
     canvas.height = backUpImg.height;
@@ -209,72 +209,135 @@ function getBlue(image, x, y) {
 /**
  *                           **********Sepia***************
  */
-document.getElementById('sepia').addEventListener('click',sepia);
+document.getElementById('sepia').addEventListener('click', sepia);
 
-function sepia(){
-	let imageData = ctx.getImageData(0, 0, w, h);
+function sepia() {
+    let imageData = ctx.getImageData(0, 0, w, h);
     let data = imageData.data;
-	for (let i = 0; i < data.length; i += 4) {
-		let r = data[i], g = data[i + 1], b = data[i + 2];
+    for (let i = 0; i < data.length; i += 4) {
+        let r = data[i], g = data[i + 1], b = data[i + 2];
 
-		data[i] = Math.min(Math.round(0.393 * r + 0.769 * g + 0.189 * b), 255);
-		data[i + 1] = Math.min(Math.round(0.349 * r + 0.686 * g + 0.168 * b), 255);
-		data[i + 2] = Math.min(Math.round(0.272 * r + 0.534 * g + 0.131 * b), 255);
-	}
-	ctx.putImageData(imageData, 0, 0);
+        data[i] = Math.min(Math.round(0.393 * r + 0.769 * g + 0.189 * b), 255);
+        data[i + 1] = Math.min(Math.round(0.349 * r + 0.686 * g + 0.168 * b), 255);
+        data[i + 2] = Math.min(Math.round(0.272 * r + 0.534 * g + 0.131 * b), 255);
+    }
+    ctx.putImageData(imageData, 0, 0);
 }
 
 /**
  *                           **********Negative***************
  */
-document.getElementById('negative').addEventListener('click',negative);
+document.getElementById('negative').addEventListener('click', negative);
 
-function negative(){
-	let imageData = ctx.getImageData(0, 0, w, h);
-	let data = imageData.data;
-	for (let i = 0; i < data.length; i += 4) {
-		data[i]     = 255 - data[i];     
-		data[i + 1] = 255 - data[i + 1]; 
-		data[i + 2] = 255 - data[i + 2];
-	}
-	ctx.putImageData(imageData, 0, 0);
+function negative() {
+    let imageData = ctx.getImageData(0, 0, w, h);
+    let data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255 - data[i + 1];
+        data[i + 2] = 255 - data[i + 2];
+    }
+    ctx.putImageData(imageData, 0, 0);
 }
 
 /**
  *                           **********Sobel***************
  */
-document.getElementById('sobel').addEventListener('click',sobel);
+document.getElementById('sobel').addEventListener('click', sobel);
 let data;
 
-function sobel(){
-    data=ctx.getImageData(0,0,w,h)
-    let data2=ctx.getImageData(0,0,w,h)
+function sobel() {
+    data = ctx.getImageData(0, 0, w, h)
+    let data2 = ctx.getImageData(0, 0, w, h)
     for (let i = 0; i < data.data.length; i += 4) {
-        let row=parseInt(i/4/w)
-        let col=parseInt(i/4%w)
-        if(row>0&&row<h){
-            if(col>0&&col<w){
-                let gx=avg(i-w*4-4)+2*avg(i-4)+avg(i+4*w-4)-avg(i-w*4+4)-2*avg(i-4)-avg(i+4*w+4);
-                let gy=avg(i-w*4-4)+2*avg(i-w*4)+avg(i+4*w+4)-avg(i+w*4-4)-2*avg(i+w*4)-avg(i+4*w+4);
-                let g=Math.sqrt(Math.pow(gx,2)+Math.pow(gy,2))
-                if(g>20){
-                    data2.data[i]=(255-g);
-                    data2.data[i+1]=(255-g);
-                    data2.data[i+2]=(255-g);
-                }else{
-                    data2.data[i]=255
-                    data2.data[i+1]=255
-                    data2.data[i+2]=255
+        let row = parseInt(i / 4 / w)
+        let col = parseInt(i / 4 % w)
+        if (row > 0 && row < h) {
+            if (col > 0 && col < w) {
+                let gx = avg(i - w * 4 - 4) + 2 * avg(i - 4) + avg(i + 4 * w - 4) - avg(i - w * 4 + 4) - 2 * avg(i - 4) - avg(i + 4 * w + 4);
+                let gy = avg(i - w * 4 - 4) + 2 * avg(i - w * 4) + avg(i + 4 * w + 4) - avg(i + w * 4 - 4) - 2 * avg(i + w * 4) - avg(i + 4 * w + 4);
+                let g = Math.sqrt(Math.pow(gx, 2) + Math.pow(gy, 2))
+                if (g > 20) {
+                    data2.data[i] = (255 - g);
+                    data2.data[i + 1] = (255 - g);
+                    data2.data[i + 2] = (255 - g);
+                } else {
+                    data2.data[i] = 255
+                    data2.data[i + 1] = 255
+                    data2.data[i + 2] = 255
                 }
             }
         }
     }
     ctx.putImageData(data2, 0, 0);
 }
-function avg(index){
-    let a=0
-    for(let i=index;i<index+3;++i){
-        a+=data.data[i]
+
+function avg(index) {
+    let a = 0
+    for (let i = index; i < index + 3; ++i) {
+        a += data.data[i]
     }
-    return a/3
+    return a / 3
+}
+
+
+/**
+ *                           **********Gray scale***************
+ */
+let btnGray = document.getElementById("gray");
+btnGray.addEventListener('click', grayScale);
+function grayScale() {
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        let grayScale = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        data[i] = grayScale;
+        data[i + 1] = grayScale;
+        data[i + 2] = grayScale;
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+
+/**
+ *                           **********To binary filter***************
+ */
+let btnBinary = document.getElementById("binary");
+btnBinary.addEventListener('click', toBinary);
+
+function toBinary() {
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        let pixel = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        if (pixel <= 127) {
+            pixel = 0;
+        } else {
+            pixel = 255;
+        }
+        data[i] = pixel;
+        data[i + 1] = pixel;
+        data[i + 2] = pixel;
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+
+/**
+ *                           **********Blur filter***************
+ */
+let btnBlur = document.getElementById("blur");
+btnBlur.addEventListener('click', blur);
+
+function blur() {
+    imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let r, g, b;
+    for (let x = 1; x < canvas.width - 1; x++) {
+        for (let y = 1; y < canvas.height - 1; y++) {
+            r = Math.floor((getRed(imageData, x, y) + getRed(imageData, x - 1, y) + getRed(imageData, x + 1, y) + getRed(imageData, x - 1, y + 1) + getRed(imageData, x - 1, y - 1) + getRed(imageData, x, y + 1) + getRed(imageData, x, y - 1) + getRed(imageData, x + 1, y + 1) + getRed(imageData, x + 1, y - 1)) / 9);
+            g = Math.floor((getGreen(imageData, x, y) + getGreen(imageData, x - 1, y) + getGreen(imageData, x + 1, y) + getGreen(imageData, x - 1, y + 1) + getGreen(imageData, x - 1, y - 1) + getGreen(imageData, x, y + 1) + getGreen(imageData, x, y - 1) + getGreen(imageData, x + 1, y + 1) + getGreen(imageData, x + 1, y - 1)) / 9);
+            b = Math.floor((getBlue(imageData, x, y) + getBlue(imageData, x - 1, y) + getBlue(imageData, x + 1, y) + getBlue(imageData, x - 1, y + 1) + getBlue(imageData, x - 1, y - 1) + getBlue(imageData, x, y + 1) + getBlue(imageData, x, y - 1) + getBlue(imageData, x + 1, y + 1) + getBlue(imageData, x + 1, y - 1)) / 9);
+
+            setPixel(imageData, x, y, r, g, b, 255);
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
 }
